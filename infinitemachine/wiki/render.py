bf6e212ce7bcdb5225a7542c321d27af46e9e06f
Matt Sparks <ms@quadpoint.org>
2010-02-06 11:07:22 -0800

    move rendering logic to render module

---
+++ infinitemachine/wiki/render.py
@@ -0,0 +1,41 @@
+import django.template.loader
+
+from infinitemachine import settings
+from infinitemachine.wiki import cache
+from infinitemachine.wiki import document
+
+
+def render(docname, template=None):
+  # load document from disk
+  doc = document.Document(docname)
+  if not doc.exists():
+    raise django.http.Http404
+
+  if template is None:
+    template = 'index.html'
+
+  if not settings.DEBUG:
+    analytics_id = getattr(settings, 'ANALYTICS_ID', None)
+  else:
+    analytics_id = None
+
+  authors = [x[0] for x in settings.MANAGERS]
+  values = {'document': doc,
+            'toplevel': docname.split('/')[0],
+            'settings': settings,
+            'title': doc.title(),
+            'title_shortname': document.header_short_name(doc.title()),
+            'site_title': getattr(settings, 'SITE_TITLE', ''),
+            'author': ', '.join(authors),
+            'analytics_id': analytics_id}
+
+  rendered = django.template.loader.render_to_string(template, values)
+  return rendered
+
+
+def cachedRender(docname, template=None):
+  rendered = cache.get(docname)
+  if rendered is None:
+    rendered = render(docname, template)
+    cache.set(docname, rendered)
+  return rendered
--- infinitemachine/wiki/views.py
+++ infinitemachine/wiki/views.py
@@ -8,49 +8,15 @@ from infinitemachine import settings
 from infinitemachine import util
 from infinitemachine.wiki import cache
 from infinitemachine.wiki import document
-
-
-@util.log_elapsed_total
-def _render(docname, template=None):
-  if template is None:
-    template = 'index.html'
-
-  # attempt to fetch document from cache
-  rendered = cache.get(docname)
-  if rendered is not None:
-    return rendered
-
-  # load document from disk
-  doc = document.Document(docname)
-  if not doc.exists():
-    raise django.http.Http404
-
-  if not settings.DEBUG:
-    analytics_id = getattr(settings, 'ANALYTICS_ID', None)
-  else:
-    analytics_id = None
-
-  authors = [x[0] for x in settings.MANAGERS]
-  values = {'document': doc,
-            'toplevel': docname.split('/')[0],
-            'settings': settings,
-            'title': doc.title(),
-            'title_shortname': document.header_short_name(doc.title()),
-            'site_title': getattr(settings, 'SITE_TITLE', ''),
-            'author': ', '.join(authors),
-            'analytics_id': analytics_id}
-
-  rendered = django.template.loader.render_to_string(template, values)
-  cache.set(docname, rendered)
-
-  return rendered
+from infinitemachine.wiki import render
 
 
 @util.log_elapsed_total
 def index(request, docname):
   logging.basicConfig(level=logging.DEBUG,
                       format='%(asctime)s - %(levelname)s - %(message)s')
-  response = django.http.HttpResponse(_render(docname), status=200)
+  rendered = render.cachedRender(docname)
+  response = django.http.HttpResponse(rendered, status=200)
   return response
 
 
